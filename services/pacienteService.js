14047c5ce6d21a85956758da9a850d645fa633db
Christian Dj <christiandj456@gmail.com>
2025-04-15 11:14:30 -0500

    Hola

--- services/pacienteService.js
+++ services/pacienteService.js
@@ -1,6 +1,30 @@
 import fetch from "node-fetch";
 import { generarToken, desencriptarRespuesta } from "../utils/jwtHelper.js";
 
+
+const IDENTIFICATIONS = [
+  { common: "Cédula de Extranjería", short: "CE", codes: [2] },
+  { common: "Tarjeta de Identidad", short: "TI", codes: [5] },
+  { common: "Cédula de Ciudadanía", short: "CC", codes: [1, 3] },
+  { common: "Registro Civil", short: "RC", codes: [4] },
+  { common: "Pasaporte", short: "PA", codes: [3] },
+  { common: "Menor Sin Identificación", short: "MSI", codes: [7] },
+  { common: "Adulto Sin Identificación", short: "ASI", codes: [6] },
+  { common: "Cédula Diplomática", short: "CD", codes: [9] },
+  { common: "Salvoconducto", short: "SC", codes: [11] },
+  { common: "Permiso Especial de Permanencia", short: "PEP", codes: [13] },
+  { common: "Permiso Temporal de Permanencia", short: "PTP", codes: [14] },
+];
+
+function homologarTipoDocumento(tipoCodigo) {
+  const tipoNum = Number(tipoCodigo);
+  const found = IDENTIFICATIONS.find(item => item.codes.includes(tipoNum));
+  return {
+    tipo_abreviado: found ? found.short : String(tipoCodigo),
+    tipo_nombre: found ? found.common : "Tipo desconocido",
+  };
+}
+
 function buildHeaders(documento) {
   const token = generarToken(documento);
   return {
@@ -9,6 +33,18 @@ function buildHeaders(documento) {
   };
 }
 
+// export async function getBasicData(documento) {
+
+//   const res = await fetch(`${process.env.URL_BASE_POBLACIONAL}/api/v1/getBasicData`, {
+//     method: "GET",
+//     headers: buildHeaders(documento),
+//   });
+
+//   if (!res.ok) throw new Error("getBasicData failed: " + res.status);
+//   const data = await res.json();
+//   return desencriptarRespuesta(data.data);
+// }
+
 export async function getBasicData(documento) {
   const res = await fetch(`${process.env.URL_BASE_POBLACIONAL}/api/v1/getBasicData`, {
     method: "GET",
@@ -16,9 +52,19 @@ export async function getBasicData(documento) {
   });
 
   if (!res.ok) throw new Error("getBasicData failed: " + res.status);
-  const data = await res.json();
-  return desencriptarRespuesta(data.data);
+  
+  const rawData = await res.json();
+  const desencriptado = desencriptarRespuesta(rawData.data);
+
+  // Homologar tipo_documento si existe
+  if (desencriptado && desencriptado.tipo_documento !== undefined) {
+    const homologado = homologarTipoDocumento(desencriptado.tipo_documento);
+    desencriptado.tipo_documento_abreviado = homologado.tipo_abreviado;
+  }
+
+  return desencriptado;
 }
+
 
 export async function getMaestra(documento) {
   const res = await fetch(`${process.env.URL_BASE_POBLACIONAL}/api/v1/maestra`, {
